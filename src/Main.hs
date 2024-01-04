77672e493685688b7e88812a2511ef4303f89005
kotuw <46974369+KotUW@users.noreply.github.com>
2024-01-04 17:20:22 +0000

    a POC

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever)
+import Control.Monad (void,  replicateM_)
 import System.Random (randomRIO)
 
 -- A list of texts to post
@@ -10,7 +10,7 @@ texts = ["Hello, world!", "How are you?", "Nice weather today.", "I'm a Haskell 
 users :: [String]
 users = ["User1", "User2", "User3", "User3", "User4", "User5", "User6", "User7", "User8"]
 postRandomText :: String -> IO ()
-postRandomText user = Control.Monad.forever $ do
+postRandomText user = do
     -- Choose a random text
     textIndex <- randomRIO (0, length texts - 1)
     let text = texts !! textIndex
@@ -18,14 +18,33 @@ postRandomText user = Control.Monad.forever $ do
     -- Print the text
     putStrLn (user ++ ": " ++ text)
 
+userThread 0 = do 
+    putStrLn "Terminating Thread."
+    
+userThread numOfMsgs = do 
+    userIndex <- randomRIO(0, length users -1)
+    let userId = users !! userIndex
+
+    postRandomText userId
     -- Wait for a random amount of time (between 1 and 5 seconds)
-    delay <- randomRIO (1, 5)
-    threadDelay (delay * 1000000)  -- threadDelay takes microseconds
+    -- delay <- randomRIO (1, 5)
+    -- threadDelay (delay * 1000000)  -- threadDelay takes microseconds
+
+    userThread (numOfMsgs - 1)
+
+-- The thread function
+threadFunction :: Int -> IO ()
+threadFunction n = replicateM_ n $ userThread 10
 
 main :: IO ()
 main = do
-    -- Create a new thread for each user
-    mapM_ (forkIO . postRandomText) users
+    putStr "Enter the number of threads to spawn:"
+    n <- readLn :: IO Int
 
-    -- Keep the main thread alive indefinitely
-    Control.Monad.forever $ threadDelay maxBound
+    -- Create the thread
+    void $ forkIO $ threadFunction n
+
+    -- Wait for the user to press enter before exiting the main thread
+    putStrLn "Press enter to exit."
+    _ <- getLine
+    return ()
